25786e7e66ee5133bf9ca61d917f15854bae6673
VTOS FOUNDATION <34971947+VTOSFOUNDATION@users.noreply.github.com>
2018-07-22 10:02:20 -0500

    Update VCTToken.sol

--- VCTToken.sol
+++ VCTToken.sol
@@ -1,205 +1,319 @@
-pragma solidity ^0.4.11;
+pragma solidity 0.4.23;
 
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
 library SafeMath {
-    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
         uint256 c = a * b;
-        assert(a == 0 || c / a == b);
+        assert(c / a == b);
         return c;
     }
 
-    function div(uint256 a, uint256 b) internal constant returns (uint256) {
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
         // assert(b > 0); // Solidity automatically throws when dividing by 0
         uint256 c = a / b;
         // assert(a == b * c + a % b); // There is no case in which this doesn't hold
         return c;
     }
 
-    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         assert(b <= a);
         return a - b;
     }
 
-    function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         assert(c >= a);
         return c;
     }
 }
 
-contract IERC20 {
-
-    function totalSupply() public constant returns (uint256);
-    function balanceOf(address who) public constant returns (uint256);
-    function transfer(address to, uint256 value) public;
-    function transferFrom(address from, address to, uint256 value) public;
-    function approve(address spender, uint256 value) public;
-    function allowance(address owner, address spender) public constant returns (uint256);
-
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+    /// Total amount of tokens
+    uint256 public totalSupply;
+  
+    function balanceOf(address _owner) public view returns (uint256);
+  
+    function transfer(address _to, uint256 _amount) public returns (bool);
+  
     event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+    function allowance(address _owner, address _spender) public view returns (uint256);
+  
+    function transferFrom(address _from, address _to, uint256 _amount) public returns (bool);
+  
+    function approve(address _spender, uint256 _amount) public returns (bool);
+  
     event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
+ */
+contract BasicToken is ERC20Basic {
+    using SafeMath for uint256;
+
+  //balance in each address account
+    mapping(address => uint256) balances;
+
+  /**
+  * @dev transfer token for a specified address
+  * @param _to The address to transfer to.
+  * @param _amount The amount to be transferred.
+  */
+    function transfer(address _to, uint256 _amount) public returns (bool) {
+        require(_to != address(0));
+        require(balances[msg.sender] >= _amount && _amount > 0 && balances[_to].add(_amount) > balances[_to]);
+
+        // SafeMath.sub will throw if there is not enough balance.
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        emit Transfer(msg.sender, _to, _amount);
+        return true;
+    }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _owner The address to query the the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return balances[_owner];
+    }
 
 }
 
-contract VCTToken is IERC20 {
+/**
+ * @title Standard ERC20 token
+ *
+ * @dev Implementation of the basic standard token.
+ * @dev https://github.com/ethereum/EIPs/issues/20
+ */
+contract StandardToken is ERC20, BasicToken {
+  
+  
+    mapping (address => mapping (address => uint256)) internal allowed;
 
-    using SafeMath for uint256;
 
-    // Token properties
-    string public name = "VTOS Community Token";
-    string public symbol = "VCT";
-    uint public decimals = 18;
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param _from address The address which you want to send tokens from
+   * @param _to address The address which you want to transfer to
+   * @param _amount uint256 the amount of tokens to be transferred
+   */
+    function transferFrom(address _from, address _to, uint256 _amount) public returns (bool) {
+        require(_to != address(0));
+        require(balances[_from] >= _amount);
+        require(allowed[_from][msg.sender] >= _amount);
+        require(_amount > 0 && balances[_to].add(_amount) > balances[_to]);
 
-    uint public _totalSupply = 0e18;
+        balances[_from] = balances[_from].sub(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_amount);
+        emit Transfer(_from, _to, _amount);
+        return true;
+    }
 
-    // Balances for each account
-    mapping (address => uint256) balances;
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   *
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   * @param _spender The address which will spend the funds.
+   * @param _amount The amount of tokens to be spent.
+   */
+    function approve(address _spender, uint256 _amount) public returns (bool) {
+        allowed[msg.sender][_spender] = _amount;
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
 
-    // Owner of account approves the transfer of an amount to another account
-    mapping (address => mapping(address => uint256)) allowed;
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param _spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return allowed[_owner][_spender];
+    }
 
-    uint256 public startTime;
+}
+/**
+ * @title Burnable Token
+ * @dev Token that can be irreversibly burned (destroyed).
+ */
+contract BurnableToken is StandardToken {
 
-    // Owner of Token
+    event Burn(address indexed burner, uint256 value);
+
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint256 _value) public {
+        require(_value <= balances[msg.sender]);
+        // no need to require value <= totalSupply, since that would imply the
+        // sender's balance is greater than the totalSupply, which *should* be an assertion failure
+
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        emit Burn(msg.sender, _value);
+    }
+}
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
     address public owner;
 
-    // how many token units a buyer gets per wei
-    // uint public PRICE = 1000;
 
-    // amount of raised money in wei
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+    constructor()public {
+        owner = msg.sender;
+    }
 
-    event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
 
-    // modifier to allow only owner has full control on the function
-    modifier onlyOwner {
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
-    // Constructor
-    // @notice VCTToken Contract
-    // @return the transaction address
-    function VCTToken() public payable {
-        owner = msg.sender;
 
-        balances[owner] = _totalSupply; 
-    }
-
-    // Payable method
-    // @notice Anyone can buy the tokens on tokensale by paying ether
-    function () public payable {
-        tokensale(msg.sender);
-    }
-
-    // @notice tokensale
-    // @param recipient The address of the recipient
-    // @return the transaction address and send the event as Transfer
-    function tokensale(address recipient) public payable {
-        require(recipient != 0x0);
-
-        // uint256 weiAmount = msg.value;
-
-        // require(_tokenLeft >= tokens);
-
-        // balances[owner] = balances[owner].sub(tokens);
-        // balances[recipient] = balances[recipient].add(tokens);
-
-        // _tokenLeft = _tokenLeft.sub(tokens);
-
-        // TokenPurchase(msg.sender, recipient, weiAmount, tokens);
-    }
-
-    // @return total tokens supplied
-    function totalSupply() public constant returns (uint256) {
-        return _totalSupply;
-    }
-
-    // What is the balance of a particular account?
-    // @param who The address of the particular account
-    // @return the balanace the particular account
-    function balanceOf(address who) public constant returns (uint256) {
-        return balances[who];
-    }
-
-    // Token distribution to founder, develoment team, partners, charity, and bounty
-    function sendVCTToken(address to, uint256 value) public onlyOwner {
-        require (
-            to != 0x0 && value > 0
-        );
-
-        balances[to] = balances[to].add(value);
-        _totalSupply = _totalSupply.add(value);
-        Transfer(owner, to, value);
-    }
-
-    function sendVCTTokenToMultiAddr(address[] listAddresses, uint256[] amount) onlyOwner {
-        require(listAddresses.length == amount.length); 
-         for (uint256 i = 0; i < listAddresses.length; i++) {
-                require(listAddresses[i] != 0x0); 
-                balances[listAddresses[i]] = balances[listAddresses[i]].add(amount[i]);
-                Transfer(owner, listAddresses[i], amount[i]);
-                _totalSupply = _totalSupply.add(amount[i]);
-         }
-    }
-
-    function destroyVCTToken(address to, uint256 value) public onlyOwner {
-        require (
-                to != 0x0 && value > 0
-            );
-        balances[to] = balances[to].sub(value);
-        _totalSupply = _totalSupply.sub(value);
-    }
-
-    // @notice send `value` token to `to` from `msg.sender`
-    // @param to The address of the recipient
-    // @param value The amount of token to be transferred
-    // @return the transaction address and send the event as Transfer
-    function transfer(address to, uint256 value) public {
-        require (
-            balances[msg.sender] >= value && value > 0
-        );
-        balances[msg.sender] = balances[msg.sender].sub(value);
-        balances[to] = balances[to].add(value);
-        Transfer(msg.sender, to, value);
-    }
-
-    // @notice send `value` token to `to` from `from`
-    // @param from The address of the sender
-    // @param to The address of the recipient
-    // @param value The amount of token to be transferred
-    // @return the transaction address and send the event as Transfer
-    function transferFrom(address from, address to, uint256 value) public {
-        require (
-            allowed[from][msg.sender] >= value && balances[from] >= value && value > 0
-        );
-        balances[from] = balances[from].sub(value);
-        balances[to] = balances[to].add(value);
-        allowed[from][msg.sender] = allowed[from][msg.sender].sub(value);
-        Transfer(from, to, value);
-    }
-
-    // Allow spender to withdraw from your account, multiple times, up to the value amount.
-    // If this function is called again it overwrites the current allowance with value.
-    // @param spender The address of the sender
-    // @param value The amount to be approved
-    // @return the transaction address and send the event as Approval
-    function approve(address spender, uint256 value) public {
-        require (
-            balances[msg.sender] >= value && value > 0
-        );
-        allowed[msg.sender][spender] = value;
-        Approval(msg.sender, spender, value);
-    }
-
-    // Check the allowed value for the spender to withdraw from owner
-    // @param owner The address of the owner
-    // @param spender The address of the spender
-    // @return the amount which spender is still allowed to withdraw from owner
-    function allowance(address _owner, address spender) public constant returns (uint256) {
-        return allowed[_owner][spender];
-    }
-
-    function getTokenDetail() public constant returns (string, string, uint256) {
-	    return (name, symbol, _totalSupply);
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+    function transferOwnership(address newOwner)public onlyOwner {
+        if (newOwner != address(0)) {
+            owner = newOwner;
+        }
     }
 }
 
+contract MintableToken is StandardToken, Ownable {
+    event Mint(address indexed to, uint256 amount);
+    event MintFinished();
+
+    bool public mintingFinished = false;
+
+
+    modifier canMint() {
+        require(!mintingFinished);
+        _;
+    }
+
+  /**
+   * @dev Function to mint tokens
+   * @param _to The address that will receive the minted tokens.
+   * @param _amount The amount of tokens to mint.
+   * @return A boolean that indicates if the operation was successful.
+   */
+    function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
+        totalSupply = totalSupply.add(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        emit Mint(_to, _amount);
+        emit Transfer(address(0), _to, _amount);
+        return true;
+    }
+
+  /**
+   * @dev Function to stop minting new tokens.
+   * @return True if the operation was successful.
+   */
+    function finishMinting() onlyOwner canMint public returns (bool) {
+        mintingFinished = true;
+        emit MintFinished();
+        return true;
+    }
+}
+
+/**
+ * @title VCT Token
+ * @dev Token representing VCT.
+ */
+contract VCTToken is BurnableToken,Ownable,MintableToken {
+    string public name ;
+    string public symbol ;
+    uint8 public decimals = 18 ;
+     
+     /**
+     *@dev users sending ether to this contract will be reverted. Any ether sent to the contract will be sent back to the caller
+     */
+    function ()public payable {
+        revert("Sending ether to the contract is not allowed");
+    }
+     
+     /**
+     * @dev Constructor function to initialize the initial supply of token to the creator of the contract
+     * @param initialSupply The initial supply of tokens which will be fixed through out
+     * @param tokenName The name of the token
+     * @param tokenSymbol The symbol of the token
+     */
+    constructor(
+        uint256 initialSupply,
+        string tokenName,
+        string tokenSymbol
+        ) public {
+        totalSupply = initialSupply * 10 ** uint256(decimals); //Update total supply with the decimal amount
+        name = tokenName;
+        symbol = tokenSymbol;
+        balances[msg.sender] = totalSupply;
+         
+        //Emitting transfer event since assigning all tokens to the creator also corresponds to the transfer of tokens to the creator
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+     
+     /**
+     * @dev allows token holders to send tokens to multiple addresses from one single transaction
+     * Beware that sending tokens to large number of addresses in one transaction might exceed gas limit of the 
+     * transaction or even for the entire block. Not putting any restriction on the number of addresses which are
+     * allowed per transaction. But it should be taken into account while creating dapps.
+     * @param dests The addresses to whom user wants to send tokens
+     * @param values The number of tokens to be sent to each address
+     */
+    function multiSend(address[]dests, uint[]values)public{
+        require(dests.length==values.length, "Number of addresses and values should be same");
+        uint256 i = 0;
+        while (i < dests.length) {
+            transfer(dests[i], values[i]);
+            i += 1;
+        }
+    }
+     
+     /**
+     *@dev helper method to get token details, name, symbol and totalSupply in one go
+     */
+    function getTokenDetail() public view returns (string, string, uint256) {
+        return (name, symbol, totalSupply);
+    }
+}
